d3da9054274b12e26737159f623457f4d4712ada
Frida Mwende <mwendehfrida@gmail.com>
2025-06-01 21:01:36 +0300

    Update index.tsx

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -1,16 +1,5 @@
 import Header from "@/components/layout/Header";
 
-const PostsPage: React.FC = () => {
-  return (
-    <div>
-      <Header />
-      <main className="p-4">
-        <h1 className="text-2xl font-bold">Posts Page</h1>
-      </main>
-    </div>
-  );
-};
-
 const Home: React.FC = () => {
   return (
     <div className="flex flex-col h-screen">
