1d5997c36849287c40b27516e38e63ebc311637b
Frida Mwende <mwendehfrida@gmail.com>
2025-06-01 21:02:44 +0300

    Update index.tsx

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/layout/Header";
+
 const UsersPage: React.FC = () => {
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">Users Page</h1>
+    <div>
+      <Header />
+      <main className="p-4">
+        <h1 className="text-2xl font-bold">Users Page</h1>
+      </main>
     </div>
   );
 };
